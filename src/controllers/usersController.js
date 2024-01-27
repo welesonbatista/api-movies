549f8d4d1b9ff66aa4c94e12bfe9d906406324d7
welesonbatista <weleson.dev@gmail.com>
2024-01-27 20:15:14 -0300

    first commit

---
+++ src/controllers/usersController.js
@@ -0,0 +1,27 @@
+const { hash, compare } = require('bcryptjs')
+const AppError = require('../utils/appError')
+const sqliteConnection = require('../database/sqlite')
+
+class UsersController {
+    async create(request, response) {
+        const { name, email, password } = request.body
+
+        const database = await sqliteConnection()
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if (checkUserExists) {
+            throw new AppError("Este e-mail já está em uso.")
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+            [name, email, hashedPassword]
+        )
+
+        return response.status(201).json()
+    }
+}
+
+module.exports = UsersController
